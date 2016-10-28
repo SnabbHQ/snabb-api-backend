f3fbbebcd4f97ec715bda344c507ffbb1c8736fa
Javier Tarazaga <javier.tarazaga@valtech.se>
2016-10-28 12:23:54 +0200

    Fixed delivery hashing

--- snabb/deliveries/models.py
+++ snabb/deliveries/models.py
@@ -9,7 +9,7 @@ from django.db import models
 def _create_hash():
     """This function generate 10 character long hash"""
     hash_value = hashlib.sha1()
-    hash_value.update(str(time.time()))
+    hash_value.update(str(time.time()).encode('utf8'))
     return hash_value.hexdigest()[:-20]
 
 
