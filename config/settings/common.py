0fc052aeb46da22109acf3ad474aecd08cfe42ea
Alberto Garcia <alberto@wearecactus.com>
2017-01-31 09:42:14 +0100

    added cors middleware

--- config/settings/common.py
+++ config/settings/common.py
@@ -58,6 +58,7 @@ INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
 MIDDLEWARE_CLASSES = (
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
