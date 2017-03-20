fa08303c038606c9844a2411b137b6a2e5aadb58
Eric Torregrosa Blasco <eric.torregrosa@wearecactus.com>
2017-03-20 10:06:19 +0100

    Register Billing App in Admin Panel

--- snabb/billing/admin.py
+++ snabb/billing/admin.py
@@ -5,7 +5,41 @@ from snabb.billing.models import(
     OrderCourier, LineOrderCourier
 )
 
-admin.site.register(OrderUser)
-admin.site.register(LineOrderUser)
-admin.site.register(OrderCourier)
-admin.site.register(LineOrderCourier)
+
+class LineOrderCourierInline(admin.StackedInline):
+    model = LineOrderCourier
+    suit_classes = 'suit-tab suit-tab-lineOrderCourier'
+    extra = 0
+
+
+class OrderCourierAdmin(admin.ModelAdmin):
+    inlines = [
+        LineOrderCourierInline
+    ]
+    model = OrderCourier
+    list_display = ['order_reference', 'total']
+    readonly_fields = ('order_id', 'total')
+    suit_form_tabs = (
+        ('general', 'General'), ('lineOrderCourier', u'Lines')
+    )
+
+
+class LineOrderUserInline(admin.StackedInline):
+    model = LineOrderUser
+    suit_classes = 'suit-tab suit-tab-lineOrderUser'
+    extra = 0
+
+
+class OrderUserAdmin(admin.ModelAdmin):
+    inlines = [
+        LineOrderUserInline
+    ]
+    model = OrderUser
+    list_display = ['order_reference', 'total']
+    readonly_fields = ('order_id', 'total')
+    suit_form_tabs = (
+        ('general', 'General'), ('LineOrderUser', u'Lines')
+    )
+
+admin.site.register(OrderCourier, OrderCourierAdmin)
+admin.site.register(OrderUser, OrderUserAdmin)
