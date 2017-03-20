0e91aa4978b43bb6c52d09a387cd5207ffff4c70
Javier Tarazaga <javier.tarazaga@valtech.se>
2017-03-20 18:33:05 +0100

    Changed repo for now for nginx

--- deploy.sh
+++ deploy.sh
@@ -11,7 +11,7 @@ deploy_image() {
 
     docker login -u $DOCKER_USERNAME -p $DOCKER_PASS -e $DOCKER_EMAIL
     docker push javiertarazaga/snabb-api-backend:$CIRCLE_SHA1 | cat # workaround progress weirdness
-    docker push snabbhq/nginx:$CIRCLE_SHA1 | cat # workaround progress weirdness
+    docker push javiertarazaga/nginx:$CIRCLE_SHA1 | cat # workaround progress weirdness
 
 }
 
