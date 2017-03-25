0fb65a45a562f0dc8b4c838713110838ee8173b6
Alberto Garcia <alberto@wearecactus.com>
2017-03-25 20:59:38 +0100

    some quote admin stuff

--- snabb/quote/admin.py
+++ snabb/quote/admin.py
@@ -5,18 +5,25 @@ from .models import Quote, Task, Place
 
 class QuoteAdmin(admin.ModelAdmin):
     model = Quote
-    list_display = ['quote_id', 'active']
+    list_display = ['quote_id', 'quote_user', 'active']
     list_filter = ['active']
     readonly_fields = ['prices']
 
 
 class TaskAdmin(admin.ModelAdmin):
     model = Task
-    list_display = ['task_id', 'active']
+    list_display = ['task_id', 'task_type', 'active']
     list_filter = ['active']
     readonly_fields = ['task_detail']
 
 
+class PlaceAdmin(admin.ModelAdmin):
+    model = Place
+    list_display = ['place_id', 'place_address',
+                    'description', 'active']
+    list_filter = ['active']
+
+
 admin.site.register(Quote, QuoteAdmin)
 admin.site.register(Task, TaskAdmin)
-admin.site.register(Place)
+admin.site.register(Place, PlaceAdmin)
