37760f08e436d09393ed2c4d4c81e8f84cd17969
Eric Torregrosa Blasco <eric.torregrosa@wearecactus.com>
2017-04-18 13:19:05 +0200

    Added tests from billing

---
+++ snabb/billing/tests/test_views.py
@@ -0,0 +1,76 @@
+import json
+from rest_framework import status
+from rest_framework.test import APITestCase, APIRequestFactory, force_authenticate
+from snabb.users.models import Profile
+from snabb.billing.views import ReceiptUserViewSet, ReceiptCourierViewSet
+
+
+class ReceiptsUserTests(APITestCase):
+
+    def create_profile(self):
+        profile = Profile.objects.get_or_create(
+            company_name='My Company S.L.', email='email@example.com',
+            password='123456', phone='+34123456789', user_lang='es'
+        )
+        return profile[0]
+
+
+    def test_get_receipts(self):
+        """ Test get receipts from a user """
+        user = self.create_profile()
+
+        factory = APIRequestFactory()
+        request = factory.get('/api/v1/billing/receiptsUser/')
+        view = ReceiptUserViewSet.as_view({'get': 'list'})
+
+        '''
+        # Without Auth -- User
+        response = view(request)
+        self.assertEqual(response.status_code, 401)
+        '''
+
+        # With Auth -- User
+        force_authenticate(request, user=user.profile_apiuser)
+        response = view(request)
+        self.assertEqual(response.status_code, 200)
+
+
+class ReceiptsCourierTests(APITestCase):
+
+    def create_profile(self):
+        profile = Profile.objects.get_or_create(
+            company_name='My Company S.L.', email='email@example.com',
+            password='123456', phone='+34123456789', user_lang='es'
+        )
+        return profile[0]
+
+
+    def test_get_receipts(self):
+        """ Test get receipts from a user """
+        user = self.create_profile()
+
+        factory = APIRequestFactory()
+        request = factory.get('/api/v1/billing/receiptsCourier/')
+        view = ReceiptCourierViewSet.as_view({'get': 'list'})
+
+
+        '''
+        # Without Auth -- Courier
+        response = view(request)
+        self.assertEqual(response.status_code, 401)
+
+        # With Auth -- Courier
+        force_authenticate(request, user=user.profile_apiuser)
+        view = ReceiptCourierViewSet.as_view({'get': 'list'})
+        response = view(request)
+        self.assertEqual(response.status_code, 401)
+        '''
+
+        # Check if user is admin
+        user.is_staff = True
+        user.is_admin = True
+        user.save()
+        force_authenticate(request, user=user.profile_apiuser)
+        view = ReceiptCourierViewSet.as_view({'get': 'list'})
+        response = view(request)
+        self.assertEqual(response.status_code, 200)
